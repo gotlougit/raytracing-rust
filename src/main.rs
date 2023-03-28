3db6ac3be27ccc71e7e247d24e6d06e92271e135
Saksham Mittal <gotlouemail@gmail.com>
2023-03-28 09:20:20 +0530

    Use file I/O to directly save file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+use std::fs;
+use std::io::Write;
+
 fn main() {
+    let filename = "out.ppm";
+    let mut file = fs::File::create(filename).unwrap();
     let width = 512;
     let height = 512;
-    println!("P3\n{} {}\n255", width,height);
+    file.write_all(format!("P3\n{} {}\n255\n", width, height).as_bytes()).unwrap();
     for j in (0..width).rev() {
+        print!("\rScanlines remaining: {}", j);
         for i in 0..width {
             let r : f64 = i as f64 / (width-1) as f64;
             let g : f64 = j as f64 / (width-1) as f64;
@@ -10,7 +16,8 @@ fn main() {
             let ir = (255.999 * r) as i32;
             let ig = (255.999 * g) as i32;
             let ib = (255.999 * b) as i32;
-            println!("{} {} {}", ir, ig, ib);
+            file.write_all(format!("{} {} {}\n", ir, ig, ib).as_bytes()).unwrap();
         }
     }
+    println!("\nDone");
 }
