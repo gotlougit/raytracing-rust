96c0499608e9424394dd8840f45b82a3dd8dcf7d
Saksham Mittal <gotlouemail@gmail.com>
2023-03-28 10:04:45 +0530

    Reuse same color object for each pixel

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,14 @@ fn main() {
     let width = 512;
     let height = 512;
     write_to_file(format!("P3\n{} {}\n255\n", width, height), &mut file);
+    let data = [0f64, 0f64, 0f64];
+    let mut color = vec3::Vec3 { data };
     for j in (0..width).rev() {
         print!("\rScanlines remaining: {}", j);
         for i in 0..width {
-            let r: f64 = i as f64 / (width - 1) as f64;
-            let g: f64 = j as f64 / (width - 1) as f64;
-            let b: f64 = 0.25;
-            let data = [r, g, b];
-            let color = vec3::Vec3 { data };
+            color.data[0] = i as f64 / (width - 1) as f64;
+            color.data[1] = j as f64 / (width - 1) as f64;
+            color.data[2] = 0.25;
             write_to_file(color.to_string(), &mut file);
         }
     }
