c9151896272101f65b9846eec29cb8f987e8ba84
Saksham Mittal <gotlouemail@gmail.com>
2023-04-02 15:26:10 +0530

    Use x,y,z instead of an array in Vec3

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,10 @@ mod ray;
 mod vec3;
 
 fn vertical_gradient(r: ray::Ray) -> vec3::Vec3 {
-    let mut data = [1f64; 3];
-    let white = vec3::Vec3 { data };
-    data = [0.5, 0.7, 1.0];
-    let blue = vec3::Vec3 { data };
+    let white = vec3::Vec3 { x: 1.0, y: 1.0, z: 1.0 };
+    let blue = vec3::Vec3 { x: 0.5, y: 0.7, z: 1.0 };
     let unitdir = vec3::unit(&r.dir);
-    let t = 0.5 * (unitdir.data[1] + 1.0);
+    let t = 0.5 * (unitdir.y + 1.0);
     white * (1.0 - t) + blue * t
 }
 
@@ -35,18 +33,11 @@ fn main() {
     let viewport_width = aspect_ratio * viewport_height;
     let focal_length = 1.0;
 
-    let mut data = [0f64; 3];
-    let origin = vec3::Vec3 { data };
-    data[0] = viewport_width;
-    let horizontal = vec3::Vec3 { data };
-    data[0] = 0.0;
-    data[1] = viewport_height;
-    let vertical = vec3::Vec3 { data };
-    data[0] = 0.0;
-    data[1] = 0.0;
-    data[2] = focal_length;
+    let origin = vec3::Vec3 { x: 0.0, y : 0.0, z: 0.0 };
+    let horizontal = vec3::Vec3 { x: viewport_width, y: 0.0, z: 0.0 };
+    let vertical = vec3::Vec3 { x: 0.0, y: viewport_height, z: 0.0 };
     let lower_left_corner =
-        origin - horizontal / 2.0 - vertical / 2.0 - vec3::Vec3 { data };
+        origin - horizontal / 2.0 - vertical / 2.0 - vec3::Vec3 { x: 0.0, y: 0.0, z: focal_length };
 
     write_to_file(format!("P3\n{} {}\n255\n", width, height), &mut file);
     for j in (0..width).rev() {
--- src/vec3.rs
+++ src/vec3.rs
@@ -3,14 +3,16 @@ use std::ops;
 
 #[derive(Clone, Copy)]
 pub struct Vec3 {
-    pub data: [f64; 3],
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
 }
 
 impl fmt::Display for Vec3 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let ir = (255.999 * self.data[0]) as i32;
-        let ig = (255.999 * self.data[1]) as i32;
-        let ib = (255.999 * self.data[2]) as i32;
+        let ir = (255.999 * self.x) as i32;
+        let ig = (255.999 * self.y) as i32;
+        let ib = (255.999 * self.z) as i32;
         write!(f, "{} {} {}\n", ir, ig, ib)
     }
 }
@@ -18,52 +20,48 @@ impl fmt::Display for Vec3 {
 impl ops::Add<Vec3> for Vec3 {
     type Output = Vec3;
     fn add(self: Vec3, _rhs: Vec3) -> Vec3 {
-        let mut newarr = [0f64; 3];
-        newarr[0] = self.data[0] + _rhs.data[0];
-        newarr[1] = self.data[1] + _rhs.data[1];
-        newarr[2] = self.data[2] + _rhs.data[2];
-        Vec3 { data: newarr }
+        let x = self.x + _rhs.x;
+        let y = self.y + _rhs.y;
+        let z = self.z + _rhs.z;
+        Vec3 { x, y, z }
     }
 }
 
 impl ops::Sub<Vec3> for Vec3 {
     type Output = Vec3;
     fn sub(self: Vec3, _rhs: Vec3) -> Vec3 {
-        let mut newarr = [0f64; 3];
-        newarr[0] = self.data[0] - _rhs.data[0];
-        newarr[1] = self.data[1] - _rhs.data[1];
-        newarr[2] = self.data[2] - _rhs.data[2];
-        Vec3 { data: newarr }
+        let x = self.x - _rhs.x;
+        let y = self.y - _rhs.y;
+        let z = self.z - _rhs.z;
+        Vec3 { x,y,z }
     }
 }
 
 impl ops::Mul<f64> for Vec3 {
     type Output = Vec3;
     fn mul(self, rhs: f64) -> Vec3 {
-        let mut newarr = [0f64; 3];
-        newarr[0] = self.data[0] * rhs;
-        newarr[1] = self.data[1] * rhs;
-        newarr[2] = self.data[2] * rhs;
-        Vec3 { data: newarr }
+        let x = self.x * rhs;
+        let y = self.y * rhs;
+        let z = self.z * rhs;
+        Vec3 { x, y, z }
     }
 }
 
 impl ops::Div<f64> for Vec3 {
     type Output = Vec3;
     fn div(self, rhs: f64) -> Vec3 {
-        let mut newarr = [0f64; 3];
-        newarr[0] = self.data[0] / rhs;
-        newarr[1] = self.data[1] / rhs;
-        newarr[2] = self.data[2] / rhs;
-        Vec3 { data: newarr }
+        let x = self.x / rhs;
+        let y = self.y / rhs;
+        let z = self.z / rhs;
+        Vec3 { x, y, z }
     }
 }
 
 impl Vec3 {
     pub fn length_squared(&self) -> f64 {
-        let x = self.data[0];
-        let y = self.data[1];
-        let z = self.data[2];
+        let x = self.x;
+        let y = self.y;
+        let z = self.z;
         return x * x + y * y + z * z;
     }
     pub fn length(&self) -> f64 {
@@ -72,35 +70,33 @@ impl Vec3 {
 }
 
 pub fn dot(v1: &Vec3, v2: &Vec3) -> f64 {
-    let x1 = v1.data[0];
-    let y1 = v1.data[1];
-    let z1 = v1.data[2];
-    let x2 = v2.data[0];
-    let y2 = v2.data[1];
-    let z2 = v2.data[2];
+    let x1 = v1.x;
+    let y1 = v1.y;
+    let z1 = v1.z;
+    let x2 = v2.x;
+    let y2 = v2.y;
+    let z2 = v2.z;
     return x1 * x2 + y1 * y2 + z1 * z2;
 }
 
 pub fn cross(v1: &Vec3, v2: &Vec3) -> Vec3 {
-    let x1 = v1.data[0];
-    let y1 = v1.data[1];
-    let z1 = v1.data[2];
-    let x2 = v2.data[0];
-    let y2 = v2.data[1];
-    let z2 = v2.data[2];
-    let mut newarr = [0f64; 3];
-    newarr[0] = y1 * z2 - z1 * y2;
-    newarr[1] = z1 * x2 - x1 * z2;
-    newarr[2] = x1 * y2 - y1 * x2;
-    Vec3 { data: newarr }
+    let x1 = v1.x;
+    let y1 = v1.y;
+    let z1 = v1.z;
+    let x2 = v2.x;
+    let y2 = v2.y;
+    let z2 = v2.z;
+
+    let x = y1 * z2 - z1 * y2;
+    let y = z1 * x2 - x1 * z2;
+    let z = x1 * y2 - y1 * x2;
+    Vec3 { x, y, z }
 }
 
 pub fn unit(v: &Vec3) -> Vec3 {
-    let x = v.data[0];
-    let y = v.data[1];
-    let z = v.data[2];
     let len = v.length();
-
-    let ud = [x / len, y / len, z / len];
-    Vec3 { data: ud }
+    let x = v.x / len;
+    let y = v.y / len;
+    let z = v.z / len;
+    Vec3 { x, y, z }
 }
